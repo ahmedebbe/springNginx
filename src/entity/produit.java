35d1b9b24a97f23dc9a12e419e56f5a25af9db8f
ebbe126 <ahmedbile126@gmail.com>
2022-07-29 16:06:17 +0000

    Mon premier commit

---
+++ src/entity/produit.java
@@ -0,0 +1,26 @@
+ 
+public class produit {
+    
+   private int id ;
+   private String nom;
+   private Double prix;
+public int getId() {
+    return id;
+}
+public String getNom() {
+    return nom;
+}
+public Double getPrix() {
+    return prix;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public void setNom(String nom) {
+    this.nom = nom;
+}
+public void setPrix(Double prix) {
+    this.prix = prix;
+} 
+
+}
